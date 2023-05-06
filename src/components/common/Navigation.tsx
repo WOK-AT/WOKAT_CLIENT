f014062ab8a215c6482baafe125c58e73010c7bb
Na Rim <skfla07@naver.com>
2023-05-06 19:10:40 +0900

    [chore] 레이아웃 마진에 따라 네비바 마진 수정

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -7,14 +7,14 @@ function Navigation() {
   const { navType, switchNavType } = useNavigation();
 
   return (
-    <div className="flex items-center justify-between rounded-[42px] bg-GRAY_50 px-[0.4rem] py-[0.4rem] mx-[1.3rem] my-[1.2rem]">
+    <div className="my-[1.2rem] flex items-center justify-between rounded-[42px] bg-GRAY_50 px-[0.4rem] py-[0.4rem]">
       {NAV_TYPE.map((data) => (
         <div
           className={`${
             navType === data
-              ? 'bg-BLUE_500 text-white font-system4_bold'
-              : 'bg-none text-GRAY_400 font-system4_medium'
-          } flex w-28 h-9 items-center justify-center rounded-[31px] px-[0.8rem] py-[1.3rem] cursor-pointer transition`}
+              ? 'bg-BLUE_500 font-system4_bold text-white'
+              : 'bg-none font-system4_medium text-GRAY_400'
+          } flex h-9 w-28 cursor-pointer items-center justify-center rounded-[31px] px-[0.8rem] py-[1.3rem] transition`}
           onClick={() => switchNavType(data)}
         >
           {data}
--- src/pages/nearby-place/index.tsx
+++ src/pages/nearby-place/index.tsx
@@ -1,10 +1,12 @@
 import profile from '@/assets/icons/profile.svg';
 import Layout from '@/components/common/Layout';
 import Map from '@/components/nearby-place/Map';
+import Navigation from '@/components/common/Navigation';
 
 function NearbyPlace() {
   return (
     <Layout title={'역삼역'} right={profile}>
+      <Navigation />
       <Map></Map>
     </Layout>
   );
