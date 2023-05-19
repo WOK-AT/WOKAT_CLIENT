d31e255d132fce4ebd648f78b3890d7a0b5080dd
leeseooo <leeseoooo@naver.com>
2023-05-19 16:42:55 +0900

    [Feat] 선택한 날짜 파란색으로 활성화

--- src/hooks/useCalendar.tsx
+++ src/hooks/useCalendar.tsx
@@ -19,32 +19,37 @@ export const useCalendar = () => {
   const { getCurrentDate } = useReservationForm();
 
   useEffect(() => {
-    const [year, month, day] = getCurrentDate(new Date());
+    const [year, month, day] = getCurrentDate(new Date(2023, 4, 1));
+    setSelectedDate({ year, month, day });
     setCurrentDate({ year, month, day });
   }, []);
 
   const onChangeMonth = (direction: string) => {
     switch (direction) {
       case 'prev':
-        setCurrentDate((cur) => {
-          const { year, month } = cur;
-          if (month - 1 < 1) {
-            return { ...cur, year: year - 1, month: 12 };
-          } else {
-            return { ...cur, month: month - 1 };
-          }
-        });
+        setCurrentDate((cur) => getPrevDate(cur));
         break;
       case 'next':
-        setCurrentDate((cur) => {
-          const { year, month } = cur;
-          if (month + 1 > 12) {
-            return { ...cur, year: year + 1, month: 1 };
-          } else {
-            return { ...cur, month: month + 1 };
-          }
-        });
+        setCurrentDate((cur) => getNextDate(cur));
         break;
+    }
+  };
+
+  const getPrevDate = (cur: DateType): DateType => {
+    const { year, month } = cur;
+    if (month - 1 < 1) {
+      return { ...cur, year: year - 1, month: 12 };
+    } else {
+      return { ...cur, month: month - 1 };
+    }
+  };
+
+  const getNextDate = (cur: DateType): DateType => {
+    const { year, month } = cur;
+    if (month + 1 > 12) {
+      return { ...cur, year: year + 1, month: 1 };
+    } else {
+      return { ...cur, month: month + 1 };
     }
   };
 
@@ -55,47 +60,100 @@ export const useCalendar = () => {
     return { lastDay, firstDayOfWeek };
   };
 
-  const renderCalendarDates = () => {
-    const { year, month } = currentDate;
-    const { lastDay: daysInMonth, firstDayOfWeek } = getMonthTotalDays(
-      year,
-      month,
-    );
-    let currentDay = 1;
-    const prevMonth =
-      month - 1 < 1
-        ? getMonthTotalDays(year - 1, 12)
-        : getMonthTotalDays(year, month - 1);
+  const selectDate = (e: React.MouseEvent) => {
+    if (e.target instanceof HTMLElement) {
+      const target = e.target.closest('td') as HTMLTableCellElement;
+      onChangeMonth(target.id);
+      setSelectedDate({ ...selectedDate, day: Number(target.innerText) });
+    }
+  };
 
-    let prevDays = prevMonth.lastDay - (firstDayOfWeek - 1);
+  const activate = (date: DateType) => {
+    const { year, month, day } = date;
+
+    return (
+      selectedDate.year === year &&
+      selectedDate.month === month &&
+      selectedDate.day === day
+    );
+  };
+
+  const renderCalendarDates = () => {
+    const {
+      year: currentYear,
+      month: currentMonth,
+      day: currentDay,
+    } = currentDate;
+    const { lastDay: daysInMonth, firstDayOfWeek } = getMonthTotalDays(
+      currentYear,
+      currentMonth,
+    );
+    let tableDataCellDay = 1;
+
+    const prev = getPrevDate(currentDate);
+    const next = getNextDate(currentDate);
+
+    let prevDays =
+      getMonthTotalDays(prev.year, prev.month).lastDay - (firstDayOfWeek - 1);
     let nextDays = 1;
     const calendarRows = [];
 
-    while (currentDay <= daysInMonth) {
+    while (tableDataCellDay <= daysInMonth) {
       const rowCells = [];
 
       for (let j = 0; j < 7; j++) {
-        if (currentDay === 1 && j < firstDayOfWeek) {
+        if (tableDataCellDay === 1 && j < firstDayOfWeek) {
           rowCells.push(
-            <td key={j} className="text-GRAY_300">
+            <td
+              key={j}
+              className={`text-GRAY_300 ${
+                activate({
+                  year: prev.year,
+                  month: prev.month,
+                  day: prevDays,
+                }) && 'h-[42px] w-[42px] rounded-full bg-BLUE_600 text-white'
+              }`}
+            >
               {prevDays++}
             </td>,
           );
-        } else if (currentDay > daysInMonth) {
+        } else if (tableDataCellDay > daysInMonth) {
           rowCells.push(
-            <td key={j} className="text-GRAY_300">
+            <td
+              key={j}
+              className={`text-GRAY_300 ${
+                activate({
+                  year: next.year,
+                  month: next.month,
+                  day: nextDays,
+                }) && 'h-[42px] w-[42px] rounded-full bg-BLUE_600 text-white'
+              }`}
+            >
               {nextDays++}
             </td>,
           );
         } else {
           rowCells.push(
-            <td key={j} className="text-GRAY_800">
-              {currentDay++}
+            <td
+              key={j}
+              className={`text-GRAY_800 ${
+                activate({
+                  year: currentYear,
+                  month: currentMonth,
+                  day: tableDataCellDay,
+                }) && 'h-[42px] w-[42px] rounded-full bg-BLUE_600 text-white'
+              }`}
+            >
+              {tableDataCellDay++}
             </td>,
           );
         }
       }
-      calendarRows.push(<tr key={currentDay}>{rowCells}</tr>);
+      calendarRows.push(
+        <tr key={tableDataCellDay} onClick={selectDate}>
+          {rowCells}
+        </tr>,
+      );
     }
     return calendarRows;
   };
