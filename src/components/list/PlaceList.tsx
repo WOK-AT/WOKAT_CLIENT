2fee1eb2760bc1de8c6413f25e4ec1cf026069a6
leeseooo <leeseoooo@naver.com>
2023-05-27 17:24:00 +0900

    [Chore] 공간 리스트 컴포넌트 스크롤 영역 세로길이 수정

--- src/components/list/PlaceList.tsx
+++ src/components/list/PlaceList.tsx
@@ -77,10 +77,7 @@ function PlaceList() {
   // const { placeList } = data;
 
   return (
-    <div
-      style={{ height: 'calc(100vh - 150px)' }}
-      className="overflow-y-scroll scrollbar-hide"
-    >
+    <div>
       {navType === '무료 회의룸' && <ReservationOption />}
 
       <section className="flex justify-between">
@@ -106,73 +103,85 @@ function PlaceList() {
         </div>
         <ListFilter currentOption={currentOption} onChange={changeOption} />
       </section>
-      {placeList.map(
-        ({ place, count, hashtags, location, imageURL }, index) => (
-          // TODO : index -> id로 변경
-          <Link
-            href={`detail/${index}`}
-            key={index}
-            className="mb-4 flex w-full border-b-2 border-GRAY_100 pb-4"
-          >
-            <div className="relative">
-              <Image
-                src={imageURL || testImage}
-                alt="place image"
-                className="h-[120px] w-[100px] overflow-hidden rounded max-[360px]:h-[100px] max-[360px]:w-[80px]"
-              />
-              <Image
-                src={bookmark}
-                alt="bookmark button"
-                className="absolute bottom-2 right-2"
-              />
-            </div>
-            <div className="ml-3 flex flex-col items-center justify-between">
-              <div className="flex-col">
-                <h1 className="mb-2 font-system3_bold text-system3_bold text-GRAY_900 max-[360px]:text-system4">
-                  {place}
-                </h1>
-                <div className="mb-1 flex">
-                  <Image
-                    src={list_location}
-                    alt="location icon"
-                    width={20}
-                    height={20}
-                  />
-                  <p className=" font-system5 text-system5 text-GRAY_600 max-[360px]:text-system6">
-                    {location}
-                  </p>
-                </div>
-                <div className="flex">
-                  <Image
-                    src={list_profile}
-                    alt="profile icon"
-                    width={20}
-                    height={20}
-                  />
-                  <p className="font-system5 text-system5 text-GRAY_600 max-[360px]:text-system6">
-                    최대 {count}명
-                  </p>
-                </div>
+
+      <div
+        style={{
+          height: `${
+            navType === '무료 회의룸'
+              ? 'calc(100vh - 247px)'
+              : 'calc(100vh - 190px)'
+          }`,
+        }}
+        className="overflow-y-scroll scrollbar-hide"
+      >
+        {placeList.map(
+          ({ place, count, hashtags, location, imageURL }, index) => (
+            // TODO : index -> id로 변경
+            <Link
+              href={`detail/${index}`}
+              key={index}
+              className="mb-4 flex w-full border-b-2 border-GRAY_100 pb-4"
+            >
+              <div className="relative">
+                <Image
+                  src={imageURL || testImage}
+                  alt="place image"
+                  className="h-[120px] w-[100px] overflow-hidden rounded max-[360px]:h-[100px] max-[360px]:w-[80px]"
+                />
+                <Image
+                  src={bookmark}
+                  alt="bookmark button"
+                  className="absolute bottom-2 right-2"
+                />
               </div>
-              <ul className="flex">
-                {hashtags.map((tag: string, index: number) => (
-                  <li
-                    key={index}
-                    className="rounded-full bg-[#E0EDFFB2] px-2 py-0.5 font-system6 text-system6 text-BLUE_600 max-[360px]:px-1 "
-                    style={{
-                      marginRight:
-                        index === hashtags.length - 1 ? '0px' : '8px',
-                    }}
-                  >
-                    <span className="mr-[3px]">#</span>
-                    {tag}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Link>
-        ),
-      )}
+              <div className="ml-3 flex flex-col items-center justify-between">
+                <div className="flex-col">
+                  <h1 className="mb-2 font-system3_bold text-system3_bold text-GRAY_900 max-[360px]:text-system4">
+                    {place}
+                  </h1>
+                  <div className="mb-1 flex">
+                    <Image
+                      src={list_location}
+                      alt="location icon"
+                      width={20}
+                      height={20}
+                    />
+                    <p className=" font-system5 text-system5 text-GRAY_600 max-[360px]:text-system6">
+                      {location}
+                    </p>
+                  </div>
+                  <div className="flex">
+                    <Image
+                      src={list_profile}
+                      alt="profile icon"
+                      width={20}
+                      height={20}
+                    />
+                    <p className="font-system5 text-system5 text-GRAY_600 max-[360px]:text-system6">
+                      최대 {count}명
+                    </p>
+                  </div>
+                </div>
+                <ul className="flex">
+                  {hashtags.map((tag: string, index: number) => (
+                    <li
+                      key={index}
+                      className="rounded-full bg-[#E0EDFFB2] px-2 py-0.5 font-system6 text-system6 text-BLUE_600 max-[360px]:px-1 "
+                      style={{
+                        marginRight:
+                          index === hashtags.length - 1 ? '0px' : '8px',
+                      }}
+                    >
+                      <span className="mr-[3px]">#</span>
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Link>
+          ),
+        )}
+      </div>
     </div>
   );
 }
