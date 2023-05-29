ee998495a26f04aaa7bac18ae85525ca71c5de69
leeseooo <leeseoooo@naver.com>
2023-05-29 14:05:56 +0900

    [Chore] Pretendard subset 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.6/variable/pretendardvariable-dynamic-subset.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
