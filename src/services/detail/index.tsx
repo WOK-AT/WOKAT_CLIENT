bc6ecf29ef8d5e992000d9a9b77baa0cc434791f
leeseooo <leeseoooo@naver.com>
2023-05-29 17:37:03 +0900

    [Chore] getPlaceDetail 오타 수정

--- src/services/detail/index.tsx
+++ src/services/detail/index.tsx
@@ -2,7 +2,7 @@ import { client } from '..';
 import { GetPlaceAddressInput } from './types';
 
 export const getPlaceDetail = async (placeId: string) => {
-  const { data } = await client.get(`/place/2/${placeId}`);
+  const { data } = await client.get(`/place/${placeId}`);
   return data;
 };
 
