9e16197bcc20d6b27ac4838cfe6cac703a8932cf
Na Rim <skfla07@naver.com>
2023-06-17 22:47:13 +0900

    [Feat] 일반 리스트 결과 없음 페이지 구현

--- src/components/list/PlaceList.tsx
+++ src/components/list/PlaceList.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import list_profile from '@/assets/icons/list_profile.svg';
 import list_location from '@/assets/icons/list_location.svg';
+import noReulst_Img from '@/assets/images/noResult.webp';
 import bookmark from '@/assets/icons/bookmark.svg';
 import ImageFallback from '../common/ImageFallback';
 import { usePlaceList } from '@/hooks/queries/usePlaceList';
@@ -31,68 +32,86 @@ function PlaceList(props: PlaceListProps) {
         }`,
       }}
     >
-      {placeList.map(
-        ({ id, place, count, hashtags = [], location, imageURL }) => (
-          <Link
-            href={`detail/${id}`}
-            key={id}
-            className="mb-4 flex w-full border-b-2 border-GRAY_100 pb-4"
-          >
-            <div className="relative">
-              <div className="flex h-[120px] w-[100px] items-center justify-center max-[360px]:h-[100px] max-[360px]:w-[80px]">
-                <ImageFallback src={imageURL} alt={place} />
-              </div>
-              <Image
-                src={bookmark}
-                alt="bookmark button"
-                className="absolute bottom-2 right-2"
-              />
-            </div>
-            <div className="ml-3 flex flex-col items-start justify-between">
-              <div className="flex-col">
-                <h1 className="mb-2 text-system3_bold font-system3_bold text-GRAY_900 max-[360px]:text-system4">
-                  {place}
-                </h1>
-                <div className="relative mb-1 flex">
-                  <Image
-                    src={list_location}
-                    alt="location icon"
-                    width={20}
-                    height={20}
-                  />
-                  <p className=" text-system5 font-system5 text-GRAY_600 max-[360px]:text-system6">
-                    {location}
-                  </p>
+      {placeList.length > 0 ? (
+        placeList.map(
+          ({ id, place, count, hashtags = [], location, imageURL }) => (
+            <Link
+              href={`detail/${id}`}
+              key={id}
+              className="flex w-full pb-4 mb-4 border-b-2 border-GRAY_100"
+            >
+              <div className="relative">
+                <div className="flex h-[120px] w-[100px] items-center justify-center max-[360px]:h-[100px] max-[360px]:w-[80px]">
+                  <ImageFallback src={imageURL} alt={place} />
                 </div>
-                <div className="flex">
-                  <Image
-                    src={list_profile}
-                    alt="profile icon"
-                    width={20}
-                    height={20}
-                  />
-                  <p className="text-system5 font-system5 text-GRAY_600 max-[360px]:text-system6">
-                    {count || '제한 없음'}
-                  </p>
-                </div>
+                <Image
+                  src={bookmark}
+                  alt="bookmark button"
+                  className="absolute bottom-2 right-2"
+                />
               </div>
-              <ul className="flex w-full">
-                {hashtags.map((tag: string, index: number) => (
-                  <li
-                    key={index}
-                    className="rounded-full bg-[#E0EDFFB2] px-2 py-0.5 text-system6 font-system6 text-BLUE_600 max-[360px]:px-1 "
-                    style={{
-                      marginRight:
-                        index === hashtags.length - 1 ? '0px' : '8px',
-                    }}
-                  >
-                    {tag}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Link>
-        ),
+              <div className="flex flex-col items-start justify-between ml-3">
+                <div className="flex-col">
+                  <h1 className="mb-2 text-system3_bold font-system3_bold text-GRAY_900 max-[360px]:text-system4">
+                    {place}
+                  </h1>
+                  <div className="relative flex mb-1">
+                    <Image
+                      src={list_location}
+                      alt="location icon"
+                      width={20}
+                      height={20}
+                    />
+                    <p className=" text-system5 font-system5 text-GRAY_600 max-[360px]:text-system6">
+                      {location}
+                    </p>
+                  </div>
+                  <div className="flex">
+                    <Image
+                      src={list_profile}
+                      alt="profile icon"
+                      width={20}
+                      height={20}
+                    />
+                    <p className="text-system5 font-system5 text-GRAY_600 max-[360px]:text-system6">
+                      {count || '제한 없음'}
+                    </p>
+                  </div>
+                </div>
+                <ul className="flex w-full">
+                  {hashtags.map((tag: string, index: number) => (
+                    <li
+                      key={index}
+                      className="rounded-full bg-[#E0EDFFB2] px-2 py-0.5 text-system6 font-system6 text-BLUE_600 max-[360px]:px-1 "
+                      style={{
+                        marginRight:
+                          index === hashtags.length - 1 ? '0px' : '8px',
+                      }}
+                    >
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Link>
+          ),
+        )
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full h-full ">
+          <Image
+            src={noReulst_Img}
+            alt="noReusltImg"
+            width={103}
+            height={132}
+            className="mb-[20px] mt-[-50px]"
+          />
+          <div className="mb-[8px] font-system4_medium text-GRAY_900">
+            주변 추천 업무 공간이 없어요.
+          </div>
+          <div className=" font-system5 text-GRAY_400">
+            다른 역을 검색하거나 지도를 이동해주세요.
+          </div>
+        </div>
       )}
     </div>
   );
