b2dff92607ad29eed15782fabe14bd2ff9191244
Na Rim <skfla07@naver.com>
2023-06-18 22:53:41 +0900

    [Chore] 삼항연산자 간단하게 변경/오타수정

--- src/components/list/PlaceList.tsx
+++ src/components/list/PlaceList.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import list_profile from '@/assets/icons/list_profile.svg';
 import list_location from '@/assets/icons/list_location.svg';
-import noReulst_Img from '@/assets/images/noResult.webp';
+import noResult_Img from '@/assets/images/noResult.webp';
 import bookmark from '@/assets/icons/bookmark.svg';
 import ImageFallback from '../common/ImageFallback';
 import { usePlaceList } from '@/hooks/queries/usePlaceList';
@@ -20,7 +20,7 @@ function PlaceList(props: PlaceListProps) {
   const { navType } = useContext(NavigationContext);
   const { data: placeList, isLoading } = usePlaceList({ station, navType });
   const router = useRouter();
-  const isNearbyPlace = router.pathname.includes('nearby-place');
+  const isListPage = router.pathname.includes('list');
 
   if (isLoading) return <Loading />;
 
@@ -99,24 +99,22 @@ function PlaceList(props: PlaceListProps) {
             </Link>
           ),
         )
-      ) : isNearbyPlace ? (
-        <div className="flex flex-col items-center justify-center h-auto">
-          <div className="mb-[8px] font-system4_medium text-GRAY_900">
-            주변 추천 업무 공간이 없어요.
-          </div>
-          <div className=" font-system5 text-GRAY_400">
-            다른 역을 검색하거나 지도를 이동해주세요.
-          </div>
-        </div>
       ) : (
-        <div className="flex flex-col items-center justify-center w-full h-full ">
-          <Image
-            src={noReulst_Img}
-            alt="noReusltImg"
-            width={103}
-            height={132}
-            className="mb-[20px] mt-[-50px]"
-          />
+        <div
+          className="flex flex-col items-center justify-center"
+          style={{
+            height: `${isListPage ? '100%' : 'auto'}`,
+          }}
+        >
+          {isListPage && (
+            <Image
+              src={noResult_Img}
+              alt="noResultImg"
+              width={103}
+              height={132}
+              className="mb-[20px] mt-[-50px]"
+            />
+          )}
           <div className="mb-[8px] font-system4_medium text-GRAY_900">
             주변 추천 업무 공간이 없어요.
           </div>
