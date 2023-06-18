d8df2d2543865df56bad30fb827b49194f0ade83
leeseooo <leeseoooo@naver.com>
2023-06-18 23:38:25 +0900

    [Chore] api endpoint 숨김 및 api response type 제네릭을 통해 추론

--- src/services/detail/types.tsx
+++ src/services/detail/types.tsx
@@ -1,3 +1,25 @@
+import { Information } from '@/types/information';
+import { OperationHours } from '@/types/operationHours';
+
+export interface GetPlaceDetailOutput {
+  status: number;
+  success: boolean;
+  message: string;
+  data: {
+    id: string;
+    category: string;
+    placeName: string;
+    distance: object;
+    count: string;
+    bookingURL: string;
+    hashtags: string[];
+    introduce: string[];
+    operationHours: OperationHours;
+    information: Information;
+    location: string;
+    imageURLs: string[];
+  };
+}
 export interface GetPlaceAddressInput {
   placeId: string;
   isRoadName: number;
