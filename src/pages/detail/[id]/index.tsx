bc9e858c6ffd1c52d03a484138d12fafb3dd4bf2
wooyoung0830 <thddndud0830@gmail.com>
2023-07-11 12:50:15 +0900

    [Fix] 공간 상세 뷰 첫 로딩시 스크롤

--- src/pages/detail/[id]/index.tsx
+++ src/pages/detail/[id]/index.tsx
@@ -1,9 +1,14 @@
 import Layout from '@/components/common/Layout';
-import PlaceInfo from '@/components/detail/PlaceInfo';
-import ImageCarousel from '@/components/detail/ImageCarousel';
+import PlaceInfo from '@/components/detail/placeInfo';
+import ImageCarousel from '@/components/detail/imageCarousel';
 import PlaceDetailInfo from '@/components/detail/PlaceDetailInfo';
+import { useEffect } from 'react';
 
 function Detail() {
+  useEffect(() => {
+    document.body.style.overflow = 'scroll';
+  }, []);
+
   return (
     <Layout>
       <ImageCarousel />
