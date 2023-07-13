ccb53980c982b6132808b3f362a04309d5cc3851
wooyoung0830 <thddndud0830@gmail.com>
2023-07-13 14:40:26 +0900

    [Feat] 공간 사진 로딩 및 에러 처리

--- src/components/detail/PlaceImage.tsx
+++ src/components/detail/PlaceImage.tsx
@@ -1,18 +1,32 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import default_place from '@/assets/images/default_place.svg';
+
+const transparent =
+  'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=';
 
 interface PlaceImageProps {
   imageURL: string;
 }
 
 function PlaceImage({ imageURL }: PlaceImageProps) {
+  const [error, setError] = useState(false);
+
+  const onError = () => {
+    setError(true);
+  };
+
   return (
     <p className="relative h-48 w-full">
       <Image
-        alt={imageURL}
-        src={imageURL}
+        src={error ? default_place : imageURL ?? default_place}
+        alt="place detail image"
         fill
         priority
+        placeholder="blur"
+        blurDataURL={`data:image/gif;base64,${transparent}`}
         style={{ objectFit: 'cover' }}
+        onError={onError}
       />
     </p>
   );
