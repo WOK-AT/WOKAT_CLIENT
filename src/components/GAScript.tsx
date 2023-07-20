86e5d27f2e85a1537fe485bc58fdfd3093fce6b5
wooyoung0830 <thddndud0830@gmail.com>
2023-07-20 13:46:39 +0900

    [Chore] Google Tag Manager id 설정

--- src/components/GAScript.tsx
+++ src/components/GAScript.tsx
@@ -10,7 +10,7 @@ function GoogleAnalytics() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_ID}');
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
       `}
       </Script>
       {/*  End Google Tag Manager */}
